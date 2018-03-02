398f3371827bffe3e807470cb499f3025d20d579
MihaiLee <yyice-time@qq.com>
2018-03-02 10:19:12 +0800

    server配置

---
+++ server_pro.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var path = require('path')
+var proxy = require('http-proxy-middleware');
+var app = express()
+app.use(express.static(path.join(__dirname + "/build")))
+
+var doubanUrl = "https://api.douban.com"
+// var url = "http://192.168.0.2:40001"
+app.use('/douban/*', proxy({ target: doubanUrl, secure: false, changeOrigin: true, pathRewrite: { '^/douban': '' } }))
+
+var zhihuUrl = "https://zhihu-daily.leanapp.cn"
+// var url = "http://192.168.0.2:40001"
+app.use('/zhihu/*', proxy({ target: zhihuUrl, secure: false, changeOrigin: true, pathRewrite: { '^/zhihu': '' } }))
+
+app.listen(4444, function () {
+    console.log("App listening at port 4444;")
+})
+
+
+// "proxy": {
+//     "/douban": {
+//       "target": "https://api.douban.com",
+//       "secure": false,
+//       "changeOrigin": true,
+//       "pathRewrite": {
+//         "^/douban": ""
+//       }
+//     },
+//     "/zhihu": {
+//       "target": "https://zhihu-daily.leanapp.cn",
+//       "secure": false,
+//       "changeOrigin": true,
+//       "pathRewrite": {
+//         "^/zhihu": ""
+//       }
+//     }
+//   }
